c8d2299f43fd3bc872cede8f8b0a7a2fb71aab31
Incubus <sbanerji@deakin.edu.au>
2018-08-13 21:30:59 +1000

    added monngo connection and sample query to find from users collection kfl db

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
+var MongoClient = require('mongodb').MongoClient
 // viewed at http://localhost:3000
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -13,6 +13,19 @@ app.get('/login', function(req, res) {
 });
 app.use(express.static(__dirname + '/public'));
 
+
+
+MongoClient.connect('mongodb://localhost:27017/animals', function (err, client) {
+    if (err) throw err;
+
+    var db = client.db('kfl');
+
+    db.collection('users').find().toArray(function (err, result) {
+        if (err) throw err;
+
+        console.log(result);
+    })
+});
 
 app.listen(3000);
 console.log('Listening on port 3000!');
