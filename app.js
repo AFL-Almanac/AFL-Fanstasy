db8b0ae2cfc49e8dc53ca49a01d28b7aad8f1e32
sukhbirsingh1104 <41713422+sukhbirsingh1104@users.noreply.github.com>
2018-08-15 13:54:24 +1000

    Add files via upload
    connection with mongo db and login from database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 var MongoClient = require('mongodb').MongoClient
 // viewed at http://localhost:3000
 app.get('/', function(req, res) {
@@ -11,21 +13,37 @@ app.get('/', function(req, res) {
 app.get('/login', function(req, res) {
     res.sendFile(path.join(__dirname + '/login.html'));
 });
-app.use(express.static(__dirname + '/public'));
+app.post('/login_user', function (req, res) {
+    var email = req.body.email;
+    var passw=req.body.password;
 
-
-
-MongoClient.connect('mongodb://localhost:27017/animals', function (err, client) {
+    MongoClient.connect('mongodb://localhost:27017', function (err, client) {
     if (err) throw err;
 
     var db = client.db('kfl');
 
-    db.collection('users').find().toArray(function (err, result) {
-        if (err) throw err;
-
-        console.log(result);
-    })
+   
+    db.collection('user-login').findOne({ email:req.body.email}, function(err, user) {
+        console.log(email)
+        if(user ===null){
+            console.log('user does not exist');
+          res.end("Login invalid");
+       }else if (user.email === req.body.email && user.password === req.body.password){
+      console.log('login successful');
+      var name= user.name;
+      res.send('welcome '+name);
+     } else {
+       console.log("Credentials wrong");
+       res.end("Login invalid");
+     }
 });
+});
+});
+app.use(express.static(__dirname + '/public'));
+
+
+
+
 
 app.listen(3000);
 console.log('Listening on port 3000!');
