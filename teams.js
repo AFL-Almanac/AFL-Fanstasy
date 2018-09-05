7ecab53b4afb32bc76cfcef50cf285c7efd54717
Sampurn <sbanerji@deakin.edu.au>
2018-09-05 15:02:07 +1000

    added route for teams in banckend for profile page

---
+++ teams.js
@@ -0,0 +1,95 @@
+module.exports  =  [
+    {
+        "seq":1,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },
+    {
+        "seq":2,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":3,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":4,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":5,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":6,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":7,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":8,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":9,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":10,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":11,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":12,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":13,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":14,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":15,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":16,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":17,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    },{
+        "seq":18,
+        "name":"Richmond",
+        "code":"RICHMOND"
+
+    }
+
+];
