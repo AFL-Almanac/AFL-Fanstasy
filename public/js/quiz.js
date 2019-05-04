9a6bc0c5ac86d6b799b89d73f2b537d147c84023
frostnovus <psolante@deakin.edu.au>
2019-05-04 11:43:36 +1000

    new page

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,69 +1,64 @@
-const questions = {
-
-    1: ["Ronnie", "Deal", "Ronnie", "Ronnie"],
-    2: ["Nolan", "Deal", "Ronnie", "Ronnie"],
-    3: ["Jekyl", "Deal", "Ronnie", "Ronnie"],
-    4: ["Hyde", "Deal", "Ronnie", "Ronnie"],
-    5: ["Ronan", "Deal", "Ronnie", "Ronnie"],
-
+const questionArray = {
+    0: ["Highest scoring goalie?"],
+    1: ["jerrie or jerry?"],
+    2: ["How are you still alive?"],
+    3: ["Who is Dumbledore?"],
+    4: ["What is Gamora?"],
 };
+const questions = {
+    0: ["Ronnie", "Deal", "Ronnie", "Ronnie"],
+    1: ["Nolan", "Deal", "Ronnie", "Ronnie"],
+    2: ["Jekyl", "Deal", "Ronnie", "Ronnie"],
+    3: ["Hyde", "Deal", "Ronnie", "Ronnie"],
+    4: ["Ronan", "Deal", "Ronnie", "Ronnie"],
+};
+var three;
+var two;
+var one;
+var i = 0;
+var itemOne = 0;
+var itemTwo = 1;
+var itemThree = 2;
+var answers = [];
+var questionsAnswered = 0;
 
+function showQuestion()
+{
+        document.getElementById("buttonOne").value = questions[i][itemOne];
+        document.getElementById("buttonTwo").value = questions[i][itemTwo];
+        document.getElementById("buttonThree").value = questions[i][itemThree];
+        document.getElementById("questionFor").innerHTML = questionArray[i];
+        one = document.getElementById("opOne").innerHTML = questions[i][itemOne];
+        two = document.getElementById("opTwo").innerHTML = questions[i][itemTwo];
+        three = document.getElementById("opThree").innerHTML = questions[i][itemThree];
+        i++;
+}
 window.onload = function ()
     {
-
-
-    //loop through questions to display in quiz page
-        for (var item in questions)
-        {
-
-            list = questions[item];
-
-            console.log(questions[item]);
-
-
-            for (var i = 0; i < list.length; i++) {
-                document.getElementById("opOne").innerHTML = list[0];
-                document.getElementById("opTwo").innerHTML = list[1];
-                document.getElementById("opThree").innerHTML = list[2];
-                answerCheck()
-
-            }
-
-        }
-
-
-
-
+            showQuestion();
     };
 
-function answerCheck(p)
-    {
-
-        console.log("working forloop");
-
-        for (var questionAnswered = 0; questionAnswered <= questions.length; questionAnswered++) {
-
-            var one = document.getElementById("opOne").innerHTML = list[0];
-            var two = document.getElementById("opTwo").innerHTML = list[1];
-            var three = document.getElementById("opThree").innerHTML = list[2];
-            console.log(one);
-            for (var item in questions)
-            {
-
-                list = questions[item];
-                if (p === "a") {
-                    var answer = one;
-                    var correct = list[3];
-
-                    if (answer === correct) {
-                        console.log("correct");
-                    }
-                }
-
-
-            }
-
-        }
-
-
+function getValue(str) {
+    answers.push(str);
+    questionsAnswered ++;
+    console.log(questionsAnswered);
+    var lengthy = Object.keys(questions).length;
+    console.log(lengthy);
+    if(questionsAnswered === questions.length){
+        document.getElementById("counting").innerText = "No more questions";
+        checkAnswers();
     }
+    else {
+        showQuestion();
+    }
+
+}
+function checkAnswers() {
+    var countedAnswers = 0;
+    for( var counter = 0; counter <= questionArray.length; counter++){
+        if(answers[counter] === questions[counter][3]){
+            countedAnswers++;
+        }
+    }
+}
+
