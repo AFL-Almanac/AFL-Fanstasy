b4c0e6b4a9214988c02a870531d03c7cd507b9f1
Faiq Ul Hassan <faiqch19@gmail.com>
2019-05-19 15:53:54 +1000

    updated

--- working/js/functions.js
+++ working/js/functions.js
@@ -1,130 +1,99 @@
 
 
-var dom = document.getElementById("container");
-var myChart = echarts.init(dom);
-var app = {};
-option = null;
-option = {
-    title : {
-        text: 'Current Trends',
-        subtext: 'Players'
-    },
-    tooltip : {
-        trigger: 'axis'
-    },
-    legend: {
-        data:['Player1','Player2','Player3','Player4','Player5']
-    },
-    toolbox: {
-        show : true,
-        feature : {
-            dataView : {show: true, title: 'Text View', lang: ['Text View','OK','Cancel'], readOnly: false},
-            magicType : {show: true, title: {line: 'Line',bar: 'Bar'},type: ['line', 'bar']},
-            //brush: {type: ['rect', 'polygon', 'lineX', 'lineY', 'keep', 'clear']},
-            restore : {show: true, title: 'Restore'},
-            saveAsImage : {show: true, title: 'Save'}
-        }
-    },
-    calculable : true,
-    xAxis : [
-        {
-            type : 'category',
-            data : ['Round 1','Round 2','Round 3','Round 4', 'Round 5', 'Round 6']
-        }
-    ],
-    yAxis : [
-        {
-            type : 'value'
-        }
-    ],
-    series : [
-        {
-            name:'Player1',
-            type:'bar',
-            data:[1,4,3,1,2,5],
-            markPoint : {
-                data : [
-                    {type : 'max', name: 'High'},
-                    {type : 'min', name: 'Low'}
-                ]
-            },
-            markLine : {
-                data : [
-                    {type : 'average', name: 'Average'}
-                ]
-            }
-        },
-        {
-            name:'Player2',
-            type:'bar',
-            data:[2,3,2,1,4,3],
-            markPoint : {
-                data : [
-                    {name : 'Average', value : 182.2, xAxis: 7, yAxis: 183},
-                    {name : 'Average', value : 2.3, xAxis: 11, yAxis: 3}
-                ]
-            },
-            markLine : {
-                data : [
-                    {type : 'average', name : 'Average'}
-                ]
-            }
-        },
-        {
-            name:'Player3',
-            type:'bar',
-            data:[5,1,4,2,3,2],
-            markPoint : {
-                data : [
-                    {name : 'Average', value : 182.2, xAxis: 7, yAxis: 183},
-                    {name : 'Average', value : 2.3, xAxis: 11, yAxis: 3}
-                ]
-            },
-            markLine : {
-                data : [
-                    {type : 'average', name : 'Average'}
-                ]
-            }
-        },
-        {
-            name:'Player4',
-            type:'bar',
-            data:[3,3,2,3,3,3],
-            markPoint : {
-                data : [
-                    {name : 'Average', value : 182.2, xAxis: 7, yAxis: 183},
-                    {name : 'Average', value : 2.3, xAxis: 11, yAxis: 3}
-                ]
-            },
-            markLine : {
-                data : [
-                    {type : 'average', name : 'Average'}
-                ]
-            }
-        },
-        {
-            name:'Player5',
-            type:'bar',
-            data:[1,2,3,4,5,1],
-            markPoint : {
-                data : [
-                    {name : 'Average', value : 182.2, xAxis: 7, yAxis: 183},
-                    {name : 'Average', value : 2.3, xAxis: 11, yAxis: 3}
-                ]
-            },
-            markLine : {
-                data : [
-                    {type : 'average', name : 'Average'}
-                ]
-            }
-        }
-    ]
-};
-;
-if (option && typeof option === "object") {
-    myChart.setOption(option, true);
+function hidefunction() {
+    
+        var y = document.getElementById("container");
+        var b = document.getElementById("BackButton");
+        var p = document.getElementById("PlayerSelectionPage");
+        var t = document.getElementById("TrendsSelectionPage");
+        y.style.display = "none";
+        b.style.display = "none";
+        p.style.display = "none";
+        t.style.display = "none";
+         
 }
 
+window.onload = hidefunction;
 
-       
- 
+function myFunction() {
+          var x = document.getElementById("Positions");
+          var y = document.getElementById("container");
+          var b = document.getElementById("BackButton");
+      var c = document.getElementById("closeButton");
+          if (x.style.display === "none") {
+            x.style.display = "block";
+        c.style.display = "block";
+            y.style.display = "none";
+            b.style.display = "none";
+          } else {
+            x.style.display = "none";
+        c.style.display = "none";
+            y.style.display = "block";
+            b.style.display = "block";
+
+          }
+        }
+
+    function viewTeam() {
+      
+      var p = document.getElementById("PlayerSelectionPage");
+      var f = document.getElementById("FeatureSelection");
+
+      if (p.style.display === "none") {
+        p.style.display = "block";
+        f.style.display = "none";
+        
+      } else {
+        p.style.display = "none";
+        f.style.display = "block";
+
+      }
+    }
+
+     function viewTrends() {
+      
+      var t = document.getElementById("TrendsSelectionPage");
+      var f = document.getElementById("FeatureSelection");
+
+      if (t.style.display === "none") {
+        t.style.display = "block";
+        f.style.display = "none";
+        
+      } else {
+        t.style.display = "none";
+        f.style.display = "block";
+
+      }
+    }
+
+    function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+
+
+   let res = document.getElementById(data).innerHTML;
+  let div = document.createElement('div');
+  div.innerHTML = res;
+  let selected = div.getElementsByClassName('img-profile');
+
+  //ev.target.style.background="white";
+
+  if (selected[0]!= null){
+    ev.target.style.border="none";
+    ev.target.style.boxShadow="0 8px 6px -6px green";
+  }
+
+  //document.getElementById(data).style.display="none";
+
+    ev.target.appendChild(selected[0]);
+
+
+}
